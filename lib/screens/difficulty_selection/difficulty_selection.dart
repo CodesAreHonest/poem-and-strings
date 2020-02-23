889fa9b2ecd8dcd7258b9018d8a09ea8e73e509c
yinghua <yinghuachai0@gmail.com>
2020-02-23 16:13:57 +0800

    [Stage Selection] - Complete

--- lib/screens/difficulty_selection/difficulty_selection.dart
+++ lib/screens/difficulty_selection/difficulty_selection.dart
@@ -1,5 +1,8 @@
 import "package:flutter/material.dart";
 
+import "./widgets/easy_button.dart";
+import "./widgets/difficult_button.dart";
+
 class DifficultySelection extends StatefulWidget {
   @override
   _DifficultySelectionState createState() => _DifficultySelectionState();
@@ -9,24 +12,50 @@ class _DifficultySelectionState extends State<DifficultySelection> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+          child: Column(
             children: <Widget>[
-              Ink(
-                  decoration: ShapeDecoration(
-                    color: Colors.lightBlue,
-                    shape: CircleBorder(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Ink(
+                      decoration: ShapeDecoration(
+                        color: Colors.lightBlue,
+                        shape: CircleBorder(),
+                      ),
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        color: Colors.white,
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/Home");
+                        },
+                      )
                   ),
-                  child: IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    color: Colors.white,
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/Home");
-                    },
-                  )
+                  SizedBox(width: 40.0),
+                  Text('选择游戏难度', style: TextStyle(
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 1.2,
+                    fontSize: 20
+                  ))
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        EasyButton(),
+                        DifficultButton(),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
---
+++ lib/screens/difficulty_selection/widgets/easy_button.dart
@@ -0,0 +1,44 @@
+import "package:flutter/material.dart";
+
+class EasyButton extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<EasyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+        onPressed: () {},
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0)
+        ),
+        textColor: Colors.white,
+        padding: EdgeInsets.all(0.0),
+        child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.0),
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: <Color>[
+                      Colors.green[800],
+                      Colors.green[700],
+                      Colors.green[600],
+                      Colors.green[500],
+                    ]
+                )
+            ),
+            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("简单", style: TextStyle(fontSize: 32)),
+                  SizedBox(height: 8.0),
+                  Text("1/10", style: TextStyle(fontSize: 16, color: Colors.white70))
+                ]
+            )
+        )
+    );
+  }
+}
