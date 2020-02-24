74e0eef138c163bff2b06ba6d6a3091a87a4c91e
yinghua <yinghuachai0@gmail.com>
2020-02-24 23:38:46 +0800

    [Difficult Stage Selection] - With Rating

--- lib/screens/difficult_stage_selection/difficult_stage_selection.dart
+++ lib/screens/difficult_stage_selection/difficult_stage_selection.dart
@@ -1,11 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:poem_and_strings/screens/difficult_stage_selection/widgets/star_display.dart';
 import "widgets/appbar.dart";
 
 class DifficultStageSelection extends StatelessWidget {
+
+  final titles = ['诗经 • 关唯', 'test', 'test', 'test',
+    'test'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DifficultStageAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: ListView.builder(
+          itemCount: titles.length,
+          itemBuilder: (context, index) {
+
+            int steps = index + 1;
+
+            return Column(
+              children: <Widget>[
+                Card( //
+                  elevation: 3.0,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.red[400],
+                      child: Text(
+                        "$steps",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400
+                        )
+                      ),
+                    ),
+                    title: Text(titles[index]),
+                    subtitle: StageRating(value: 1),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                    onTap: () {},
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 16.0
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8.0)
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/difficult_stage_selection/widgets/star_display.dart
@@ -0,0 +1,46 @@
+import "package:flutter/material.dart";
+
+class StarDisplay extends StatelessWidget {
+
+  final int value;
+  final Widget filledStar;
+  final Widget unfilledStar;
+  const StarDisplay({
+    Key key,
+    this.value = 0,
+    @required this.filledStar,
+    @required this.unfilledStar,
+  })  : assert(value != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(3, (index) {
+        return index < value ? filledStar : unfilledStar;
+      }),
+    );
+  }
+}
+
+class StageRating extends StatelessWidget {
+
+  final int value;
+  const StageRating({this.value = 0}) : assert(value != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(
+        color: Colors.amber,
+        size: 16,
+      ),
+      child: StarDisplay(
+        value: value,
+        filledStar: Icon(Icons.star),
+        unfilledStar: Icon(Icons.star_border),
+      ),
+    );
+  }
+}
