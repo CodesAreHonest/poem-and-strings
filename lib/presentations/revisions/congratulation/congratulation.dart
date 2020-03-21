5f30db40ebbf5f19681ab57fed8e1f7d56024f37
yinghua <yinghuachai0@gmail.com>
2020-03-21 22:05:13 +0800

    [Success Page] - Completed

--- lib/presentations/revisions/congratulation/congratulation.dart
+++ lib/presentations/revisions/congratulation/congratulation.dart
@@ -3,32 +3,56 @@ import 'package:flutter/painting.dart';
 
 class Congratulation extends StatelessWidget {
   Widget successContainer(context) {
+    Widget nextActivityButton = RaisedButton(
+      onPressed: () {},
+      color: Colors.blue[700],
+      padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 16.0),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+          side: BorderSide(color: Colors.blue[800])),
+      child: Text('下一个活动', style: TextStyle(color: Colors.white, fontSize: 16)),
+    );
+
+    Widget backToHomeButton = FlatButton(
+        onPressed: () {},
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 16.0),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        child: Text('回到主页',
+            style: TextStyle(color: Colors.blue[800], fontSize: 16)));
+
     return Positioned(
       child: Container(
           width: MediaQuery.of(context).size.width,
-          padding: EdgeInsets.all(8.0),
+          padding: EdgeInsets.fromLTRB(8.0, 40.0, 8.0, 8.0),
           height: 300,
           decoration: BoxDecoration(
               color: Colors.white, borderRadius: BorderRadius.circular(16.0)),
-          child: Text('hello from the other side')),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 8.0),
+              Text('恭喜闯关成功',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold)),
+              SizedBox(height: 46.0),
+              nextActivityButton,
+              SizedBox(height: 8.0),
+              backToHomeButton
+            ],
+          )),
     );
   }
 
-  Widget relativeContainer(context) {
-    Widget image = Positioned(
+  Widget trophieImage(context) {
+    return Positioned(
       bottom: 230,
       child: Container(
           width: MediaQuery.of(context).size.width,
           child: Center(
               child: Image.asset('assets/icon/trophie.png',
                   width: 250, height: 250))),
-    );
-
-    return Container(
-      child: Stack(overflow: Overflow.visible, children: <Widget>[
-        successContainer(context),
-        image,
-      ]),
     );
   }
 
@@ -44,7 +68,14 @@ class Congratulation extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[relativeContainer(context)],
+          children: <Widget>[
+            Container(
+              child: Stack(overflow: Overflow.visible, children: <Widget>[
+                successContainer(context),
+                trophieImage(context),
+              ]),
+            )
+          ],
         ),
       ),
     )));
